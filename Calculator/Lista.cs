2cd7dc3aca1865db4b5231eedc159f90f4e09af3
Fredrik Törnberg <f.tornberg@me.com>
2021-10-12 19:48:33 +0200

    Kommenterade all kod.

--- Calculator/Lista.cs
+++ Calculator/Lista.cs
@@ -1,12 +1,12 @@
 ﻿namespace Calculator
 {
-    class Lista
+    /*class Lista                                             // Skapar en klass för variabler som ska matas in i listan.
     {
 
-        public float Value1 { get; set; }
+        public float Value1 { get; set; }                   // Ta emot värdet och sätt Value1
         public string Operand { get; set; }
         public float Value2 { get; set; }
         public string Likamed { get; set; }
         public float Sum { get; set; }   
-    }
+    }*/
 }
